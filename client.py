913109ecede2f10c540940d97a52d5bb0cd7f7bb
kokinomu-blip <kokinomu@gmail.com>
2026-04-17 18:01:59 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 
 sock = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
 
@@ -8,28 +10,65 @@ username =  username.encode()
 usernamelen = len(username).to_bytes(1 , "big")
 
 # input address
-server_address = input("Type in the sever's addressn to connect to: ")
+server_address = input("Type in the sever's address to connect to: ")
 server_port = 9001
 
-# input contents
-contents = input('Typing some messages : ')
-contents = contents.encode()
+#event for stopping tread
+stop_event = threading.Event()
 
-message = usernamelen + username + contents
-messagelen = len(message)
-if messagelen > 4096 :
-    print()
+def recv_loop() :
+    sock.settimeout(1)
+    while not stop_event.is_set() :
+        try :
+            data, sever = sock.recvfrom(4096)
+            print('{!r}'.format(data))
 
-try :
-    print('sending {!r}'.format(message))
+        except socket.timeout :
+            continue
 
-    sent = sock.sendto(message, (server_address , server_port))
-    print('send {} byte'.format(sent))
+        except OSError as e:
+            print(e)
+            stop_event.set()
 
-    print('waiting to receive')
-    data, sever = sock.recvfrom(4096)
-    print('receive {!r}'.format(data))
+        except Exception as e:
+            print(e)
+            continue
 
-finally:
-    print('closing socket')
-    sock.close()
+    print("stopped receiving")
+
+
+def send_loop() : 
+    while not stop_event.is_set() :
+        # input contents
+        contents = input('Typing some messages : ')
+
+        #if input 'q', stop chatting
+        if contents.lower() == 'q':
+            print('stopped sending message')
+            stop_event.set()
+            break
+
+        print('{!r}'.format(contents))
+
+        #sending message
+        contents = contents.encode()
+        message = usernamelen + username + contents
+        sent = sock.sendto(message, (server_address , server_port))
+
+
+
+thread_recv = threading.Thread(target=recv_loop)
+thread_send = threading.Thread(target=send_loop)
+
+#start tread
+thread_recv.start()
+thread_send.start()
+
+thread_recv.join()
+thread_send.join()
+
+#close socket
+print('closing socket')
+sock.close()
+
+
