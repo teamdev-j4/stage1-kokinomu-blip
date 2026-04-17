eb6b3b56330a03a6526de96dd6bce5f2c0b8b5a8
kokinomu-blip <kokinomu@gmail.com>
2026-04-17 18:02:36 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,42 +1,55 @@
 import socket
 import time
+import threading
 
 # make UDP socket
 sock = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
 
+#setup
 server_address = '0.0.0.0'
 server_port = 9001
 print('starting up on port {}'.format(server_port))
 
-socket.bind((server_address , server_port))
+sock.bind((server_address , server_port))
 
 clients = {}
 timeout = 10
+lock = threading.Lock()
 
-while True:
-    print('\nwaiting to receive message')
-    data, address = sock.recvfrom(4096)
+def tread_relay() :
+    while True:
+        print('\nwaiting to receive message')
+        data, address = sock.recvfrom(4096) #receive message
+        client = address
 
-    client = address
+        print('receive {} bytes from {}'.format(len(data) , address))
+        print(data)
 
-    now = time.time()
+        with lock :
+            clients[address] = time.time()
 
-    print('receive {} bytes from {}'.format(len(data) , address))
-    print(data)
+        if data:
+            with lock :
+                latest_clients = clients.copy()
 
-    clients[address] = time.time()
+            for client in latest_clients.keys() :
+                if client == address :
+                    continue
+                    
+                sent = sock.sendto(data, client)
 
-    if data:
-        for i in list(clients.keys()) :
-            if client == address :
-                continue
-                
-            sent = sock.sendto(data, client)
-
-    #session timeout
+def thread_timeout() :
+#session timeout
     while True :
         time.sleep(10)
-        for client in list(clients.key()) :
-            if now - clients[client] > timeout :
-                del clients[client]
-        
+        now = time.time()
+        with lock :
+                latest_clients = clients.copy()
+        for client in latest_clients :
+            if now - latest_clients[client] > timeout :
+                with lock :
+                    if client in clients :
+                        del clients[client]
+
+thread_relay = threading.Thread(target=tread_relay)
+thread_timeout = threading.Thread(target=thread_timeout)
